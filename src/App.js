342b036753ac022d51889b323e86a0c25851e826
Abellybaba <62292802+Abellybaba@users.noreply.github.com>
2022-12-11 19:01:10 -0500

    initial commit add tailwind and element js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-6xl font-bold underline bg-slate-300 h-screen">
+      Hello New world!
+    </h1>
+    
+    </div>
+  );
+}
+
+export default App;
